732b835938a7a80fe3c5ef71b84fa2ce0090bd01
fredrik@DOLDA2000.COM <fredrik@DOLDA2000.COM@959494ce-11ee-0310-bf91-de5d638817bd>
2005-04-29 18:44:55 +0000

    Added LGPL notice and compile-command.
    git-svn-id: svn+ssh://svn.dolda2000.com/srv/svn/repos/src/icmp-dn@232 959494ce-11ee-0310-bf91-de5d638817bd

--- nss-icmp.c
+++ nss-icmp.c
@@ -1,3 +1,24 @@
+/*
+ *  nss-icmp or libnss_icmp - GNU C Library NSS module to query host
+ *  names by ICMP.
+ *  Copyright (C) 2005 Fredrik Tolf <fredrik@dolda2000.com>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ *  MA 02111-1307, USA
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -367,3 +388,9 @@ enum nss_status _nss_icmp_gethostbyaddr_r(const void *addr, socklen_t len, int a
     DEBUGP("returning\n");
     return(NSS_STATUS_SUCCESS);
 }
+
+/*
+ * Local Variables:
+ * compile-command: "gcc -shared -Wall -g -o libnss_icmp.so.2 nss-icmp.c"
+ * End:
+ */
