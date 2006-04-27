29183b092874493c6585c4c0c3caf19683436945
fredrik <fredrik@959494ce-11ee-0310-bf91-de5d638817bd>
2006-04-27 15:22:50 +0000

    Factored ICMP structs and defs into icmpdefs.h.
    git-svn-id: svn+ssh://svn.dolda2000.com/srv/svn/repos/src/icmp-dn@600 959494ce-11ee-0310-bf91-de5d638817bd

--- src/idnlookup.c
+++ src/idnlookup.c
@@ -35,32 +35,7 @@
 #include "config.h"
 #endif
 
-struct icmphdr {
-    u_int8_t type;
-    u_int8_t code;
-    u_int16_t checksum;
-};
-
-struct reqhdr {
-    u_int8_t type;
-    u_int8_t code;
-    u_int16_t checksum;
-    u_int16_t id;
-    u_int16_t seq;
-};
-
-struct rephdr {
-    u_int8_t type;
-    u_int8_t code;
-    u_int16_t checksum;
-    u_int16_t id;
-    u_int16_t seq;
-    int32_t ttl;
-    
-};
-
-#define ICMP_NAMEREQ 37
-#define ICMP_NAMEREP 38
+#include "icmpdefs.h"
 
 unsigned char buf[65536];
 
